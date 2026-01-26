426d405b42bdd72a332ca1f56ffeb43b509364a6
NichlasB <apps@mailastic.com>
2026-01-26 22:46:13 +0100

    Add GitHub Plugin URI to plugin header
    Included the GitHub Plugin URI field in the plugin header to specify the repository location. This helps with plugin management and updates from GitHub.

--- alynt-certificate-generator.php
+++ alynt-certificate-generator.php
@@ -10,6 +10,7 @@
  * Domain Path: /languages
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI: NichlasB/alynt-certificates-generator
  *
  * @package AlyntCertificateGenerator
  */
